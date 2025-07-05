954909a4edce842bf4219fe2e9e41346a9741b50
gusDev <joaogustavolima084@gmail.com>
2025-07-05 16:09:14 -0300

    feat: create usuario class and attribute

--- src/model/Motorista.java
+++ src/model/Motorista.java
@@ -1,5 +1,18 @@
 package model;
 
-public class Motorista {
-    
+public class Motorista extends Usuario{
+    private String setor;
+    private String cnh;
+
+    public Motorista(String nome, String senha,String setor, String cnh){
+        super(nome, senha, false);
+        this.setor = setor;
+        this.cnh = cnh;
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + " setor:" + this.setor + " cnh:" + this.cnh;
+    }
+
 }
