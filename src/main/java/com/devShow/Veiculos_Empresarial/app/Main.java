5372d85f0d5cd03f769a26a5c2c47a5fbfbdcb7d
gus <joaogustavolima084@gmail.com>
2025-07-15 21:02:00 -0300

    feat: menu administrativo motorista

--- src/main/java/com/devShow/Veiculos_Empresarial/app/Main.java
+++ src/main/java/com/devShow/Veiculos_Empresarial/app/Main.java
@@ -15,10 +15,12 @@ public class Main{
         String nome, username, senha, setor, cnh;
         int opcao;
         do {
-            System.out.print("===== MENU =====\n1. REGISTRO\n2. LOGIN\n>>");
+            limparTela();
+            System.out.print("===== MENU =====\n1. REGISTRO\n2. LOGIN\n0. DESLIGAR\n>>");
             opcao = input.nextInt();
             input.nextLine();
             if(opcao == 1){
+                limparTela();
                 System.out.println("CADASTRO ADMINISTRAÇÃO\nNOME:");
                 nome = input.nextLine();
                 System.out.println("USERNAME:");
@@ -28,6 +30,7 @@ public class Main{
                 senha = input.next();
                 usuarioService.cadastrarUsuario(nome, username, senha);
             }else if(opcao == 2){
+                limparTela();
                 System.out.println("===LOGIN===\nUSERNAME:");
                 username = input.next();
                 input.nextLine();
@@ -36,6 +39,7 @@ public class Main{
                 Usuario novoLogin = usuarioService.autenticar(username, senha);
 
                 if(novoLogin != null){
+                    limparTela();
                     System.out.println("Login Bem-sucedido!");
                     if(novoLogin.getEhAdm()){
                         menuAdmin(novoLogin);
@@ -43,6 +47,10 @@ public class Main{
                         menuMotorista();
                     }
                 }
+            }else if(opcao == 0){
+                System.out.println("Desligando...");
+            }else{
+                System.err.println("ERRO: opcao invalida");
             }
         } while (opcao != 0);
     }
@@ -54,14 +62,26 @@ public class Main{
         int opcao;
         Scanner input = new Scanner(System.in);
         do {
-            System.out.println("AREA ADMINISTRATIVA\n1. Gerenciamento de Motoristas\n2. Gerenciar Veiculos\n3. Controlar manutenção\n4. Visualizar Registros de uso");
+            System.out.println("AREA ADMINISTRATIVA\n1. Gerenciamento de Motoristas\n2. Gerenciar Veiculos\n3. Controlar manutenção\n4. Visualizar Registros de uso\n0. Sair");
             opcao = input.nextInt();
             if(opcao == 1){
+                limparTela();
                 menuGerenciamentoDeMotorista(admin);
             }else if(opcao == 2){
+                limparTela();
                 menuGerenciamentoVeiculos();
             }else if(opcao == 3){
-
+                limparTela();
+                menuControleDeManutencao();
+            }else if(opcao == 4){
+                limparTela();
+                menuRegistros();
+            }else if(opcao == 0){
+                limparTela();
+                break;
+            }else{
+                limparTela();
+                System.err.println("ERRO: opcao invalida");
             }
         } while (opcao != 0);
         
@@ -79,10 +99,11 @@ public class Main{
         String nome, username, senha, setor, cnh;
         UsuarioService usuarioService = new UsuarioService();
         do {
-            System.out.print("Gerenciamento de Motoristas\n1. CADASTRAR NOVO MOTORISTA\n2. EDITAR INFORMAÇÕES\n3. LISTAR MOTORISTAS\n4. REMOVER MOTORISTA\n>>");
+            System.out.print("Gerenciamento de Motoristas\n1. CADASTRAR NOVO MOTORISTA\n2. EDITAR INFORMAÇÕES\n3. LISTAR MOTORISTAS\n4. REMOVER MOTORISTA\n0. VOLTAR\n>>");
             opcao = input.nextInt();
             input.nextLine();
             if(opcao == 1){
+                limparTela();
                 System.out.print("NOME:");
                 nome = input.nextLine();
                 System.out.print("USERNAME:");
@@ -96,6 +117,33 @@ public class Main{
                 cnh = input.next();
                 input.nextLine();
                 usuarioService.cadastrarMotorista(admin, nome, username, senha, setor, cnh);
+            }else if(opcao == 2){
+                limparTela();
+                System.out.print("CNH DO MOTORISTA:");
+                cnh = input.next();
+                input.nextLine();
+                System.out.print("NOME:");
+                nome = input.nextLine();
+                System.out.print("USERNAME:");
+                username = input.next();
+                input.nextLine();
+                System.out.print("SENHA:");
+                senha = input.nextLine();
+                System.out.print("SETOR:");
+                setor = input.nextLine();
+                usuarioService.editarMotorista(admin, nome, username, senha, setor, cnh);
+            }else if(opcao == 3){
+                limparTela();
+                usuarioService.listarMotoristas(admin);
+            }else if(opcao == 4){
+                limparTela();
+                System.out.println("falta implementar");
+            }else if(opcao == 0){
+                limparTela();
+                break;
+            }else{
+                limparTela();
+                System.err.println("ERRO: opcao invalida");
             }
         } while (opcao != 0);
 
@@ -113,6 +161,20 @@ public class Main{
 
     public static void menuRegistros(){
 
+    }
+
+    public static void limparTela() {
+        try {
+            String os = System.getProperty("os.name").toLowerCase();
+
+            if (os.contains("win")) {
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            } else {
+                new ProcessBuilder("clear").inheritIO().start().waitFor();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
